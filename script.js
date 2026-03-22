cd6f10f7354e7c8bbf85a39b08483a6a99875387
Prasad Kakad <prasadkakad118@gmail.com>
2026-03-22 12:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let currentAudio = null;
 buttons.forEach(btn => {
   btn.addEventListener("click", () => {
 
-    // Stop button
     if (btn.classList.contains("stop")) {
       if (currentAudio) {
         currentAudio.pause();
@@ -16,14 +15,16 @@ buttons.forEach(btn => {
 
     const sound = btn.getAttribute("data-sound");
 
-    // Stop previous sound
     if (currentAudio) {
       currentAudio.pause();
-      currentAudio.currentTime = 0;
+      currentAudio.remove();
     }
+    const audio = document.createElement("audio");
+    audio.src = `sounds/${sound}.mp3`;
+    audio.autoplay = true;
 
-    // Play new sound
-    currentAudio = new Audio(`sounds/${sound}.mp3`);
-    currentAudio.play();
+    document.body.appendChild(audio);
+
+    currentAudio = audio;
   });
 });
